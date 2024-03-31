3f905b93f594591a074c13a989f7cb7d32106d2c
rsystems-8601 <pradeep.srivastav@rsystems.com>
2024-04-01 00:40:35 +0530

    add sh

--- js-snippet/G-CI-CD-Pipeline.js
+++ js-snippet/G-CI-CD-Pipeline.js
@@ -1,3 +1,27 @@
 https://www.youtube.com/watch?v=3jXtTSnA8zw&t=10s
 // CI=> COntinous Integration
 // CD=> COntinuous Deployment
+action=> create workflow
+setting => action / action runner 
+sch.sh 
+cd /etc/nginx
+cd sites-available
+
+sudo nano default
+ip:80
+ip:80/api/
+ip:8000/api 
+============
+
+
+location /api/{
+    proxy_pass http://localhost:8000/;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for
+}
+===================
+:wq then exit
+nano default
+sudo nano default
+sudo systemctl restart nginx 
